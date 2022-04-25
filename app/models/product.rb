67d4b5eb07acaa21791986afb3dd1c57af539547
Alex Lutz <alexinslc@gmail.com>
2022-04-25 17:21:05 -0600

    validate that the product title is at least 10 characters

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
 
   validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { minimum: 10 }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'

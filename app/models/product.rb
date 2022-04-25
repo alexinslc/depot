1273dac0219e464284615497ba96f84985685b8c
Alex Lutz <alexinslc@gmail.com>
2022-04-25 16:46:50 -0600

    Adds basic validation to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end

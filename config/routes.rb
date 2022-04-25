8b375c1f9aeeb826ce01a4861f65bfa6f2ae2fab
Alex Lutz <alexinslc@gmail.com>
2022-04-25 17:39:17 -0600

    setup default route for store index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'store/index'
+  root 'store#index', as: 'store_index'
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end

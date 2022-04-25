d780d73db0de34abe77d7c4443f102ca5f888aaa
Alex Lutz <alexinslc@gmail.com>
2022-04-25 17:27:27 -0600

    custom message for validating presence of all fields

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
 
-  validates :title, :description, :image_url, presence: true
+  validates :title, :description, :image_url, presence: { message: 'cannot be blank' }
   validates :title, uniqueness: true, length: { minimum: 10 }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,

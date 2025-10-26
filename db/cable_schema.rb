3c2f0ab6045f2d633d0dba099c4f8088fd453a77
alexinslc <alexinslc@gmail.com>
2025-10-26 11:52:05 -0600

    Fix RuboCop offense in cable_schema

--- db/cable_schema.rb
+++ db/cable_schema.rb
@@ -1,7 +1,7 @@
 ActiveRecord::Schema[7.1].define(version: 1) do
   create_table "solid_cable_messages", force: :cascade do |t|
     t.binary "channel", limit: 1024, null: false
-    t.binary "payload", limit: 536870912, null: false
+    t.binary "payload", limit: 536_870_912, null: false
     t.datetime "created_at", null: false
     t.integer "channel_hash", limit: 8, null: false
     t.index ["channel"], name: "index_solid_cable_messages_on_channel"
